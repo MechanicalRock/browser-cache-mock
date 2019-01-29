bc6dbb5c417e384b6bcb6f5ee3732202f3a0c281
Rick Foxcroft <warwickfoxcroft@gmail.com>
2019-01-29 12:50:02 +0800

    Update types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -7,10 +7,10 @@ export default class CacheMock {
     add(request: RequestInfo): Promise<void>;
     addAll(requests: RequestInfo[]): Promise<void>;
     delete(request: RequestInfo, options?: CacheQueryOptions): Promise<boolean>;
-    getRequestUrl(request: RequestInfo, options?: CacheQueryOptions): string;
+    private getRequestUrl;
     keys(request?: RequestInfo, options?: CacheQueryOptions): Promise<ReadonlyArray<Request>>;
     match(request: RequestInfo, options?: CacheQueryOptions): Promise<Response | undefined>;
-    validateRequest(request: RequestInfo, options?: CacheQueryOptions): Request;
+    private validateRequest;
     matchAll(request?: RequestInfo, options?: CacheQueryOptions): Promise<ReadonlyArray<Response>>;
     put(request: RequestInfo, response: Response): Promise<void>;
 }
